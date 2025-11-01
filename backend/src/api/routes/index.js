fbc5e6b7a4525552fc2ac5cff86079bccc3f7f31
aashima2693 <aashima1976@gmail.com>
2025-11-01 05:59:56 +0530

    loan modulee

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -4,6 +4,8 @@ import { Router } from 'express';
 import authRouter from './auth.routes.js';
 import userRouter from './user.routes.js';
 import tripRouter from './trip.routes.js';
+import loanRouter from "./loan.routes.js";
+
 
 const router = Router();
 
@@ -20,5 +22,6 @@ router.get('/health', (req, res) => {
 router.use('/auth', authRouter);     // Routes for /api/v1/auth
 router.use('/users', userRouter);    // Routes for /api/v1/users
 router.use('/trips', tripRouter);    // Routes for /api/v1/trips
+router.use("/loans", loanRouter);   // Routes for /api/v1/loans
 
 export default router;
