773c6699504247615686477cde35bc80881760fc
aashima2693 <aashima1976@gmail.com>
2025-11-01 08:42:11 +0530

    loan updated1

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,15 +1,16 @@
-// TRIPMATE-/backend/config/db.config.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB Connected Successfully.');
-    } catch (err) {
-        console.error(`MongoDB Connection Error: ${err.message}`);
-        process.exit(1); // Exit process with failure
-    }
-};  
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB connection failed: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
